a094e0a36a836305d86231f8ec56256a1b0bca99
Jamon Dixon <jamonadixon@gmail.com>
2021-10-28 12:39:01 -0500

    finished logic

--- functions.js
+++ functions.js
@@ -21,26 +21,15 @@ function get_number(prompt){
     return(initial_number);
 }
 
-// function init_guesses() {
-//     let guesses = []
-//     for(let i = 0; i < pickedNumber; i++) {
-//         guesses.push();
-//     }
 
-//     return rolls;
-// }
-
-
-// let num = Math.floor(Math.random() * 25) + 1;
 console.log(num)
 var count = 0
 let guessArray = []
 
+
 let previousGuess = document.createElement('h4');
 previousGuess.textContent = '';
 document.body.appendChild(previousGuess);
-
-
 
 
 function do_guess() {
@@ -50,12 +39,12 @@ function do_guess() {
 
     if(guess == num) {
         count ++;
-        guessArray.push(guess);
+        if (guessArray.includes(guess) === false)guessArray.push(guess);
         message.innerHTML = "You Got it! It took you " + guessArray.length + " tries! Your guesses were " + guessArray
     } else if (guess > num &&  guess <= pickedNumber){
         message.innerHTML = "No try a lower number.";
         count ++;
-        guessArray.push(guess);
+        if (guessArray.includes(guess) === false)guessArray.push(guess);
         console.log(guessArray)
     } else if (isNaN(guess)) {
         message.innerHTML = "That is not a number!"
@@ -64,42 +53,11 @@ function do_guess() {
     } else if (guess < num && guess <= pickedNumber){
         message.innerHTML = "No try a higher number.";
         count ++;
-        guessArray.push(guess);
+        if (guessArray.includes(guess) === false)guessArray.push(guess);
         console.log(guessArray)
     }else {
         message.innerHTML = "That number is not in range, try again!"
     }
 }
 
-guess < num && guess <= pickedNumber
-
-
-// function guessArray(n){
-//     var newArray = [];
-//     for(var i=0; i <= 25; i++){
-//         newArray.push(n[i]);
-//     }
-//     return newArray;
-// }
-
-// not working
-// function do_guess() {
-    
-//     let guess = Number(document.getElementById("guess").value);
-//     let message = document.getElementById("message");
-// switch(guess) {
-//     case 1: (isNaN(guess) )
-//         message.innerHTML = "That is not a number!";
-//     case 2: (guess < 1 || guess > 25)
-//         message.innerHTML = "That is out of range";
-//     case 3: (guess > num && guess < 25)
-//         message.innerHTML = "Guess a lower number";
-//         count++;
-//     case 4: (guess < num && guess > 1)
-//         message.innerHTML = "Guess a higher number";
-//         count++;
-//     case 5: (guess === num)
-//         count++;
-//         message.innerHTML = "You got it! It took you " + count + " tries!"
-//     }
-// }
+// guess < num && guess <= pickedNumber
