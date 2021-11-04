bc9ace06e753a260d717043f19c85a040655b8e8
Jamon Dixon <jamonadixon@gmail.com>
2021-11-04 14:15:24 -0500

    cube spinning

--- three.js
+++ three.js
@@ -1,27 +1,56 @@
-import * as THREE from 'three';
 
 
-const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// We need 3 things everytime we use Three.js
+ // Scene + Camera + Renderer
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.2, 1000 )
+const renderer = new THREE.WebGLRenderer({ antialias: true})
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+renderer.setSize( window.innerWidth, window.innerHeight )
+// sets renderer background color
+renderer.setClearColor("#ffffff")
+document.body.appendChild( renderer.domElement )
+camera.position.z = 5
 
-			const geometry = new THREE.BoxGeometry();
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+// resize canvas on resize window
+window.addEventListener( 'resize', () => {
+    let width = window.innerWidth
+    let height = window.innerHeight
+    renderer.setSize( width, height )
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+})
 
-			camera.position.z = 5;
+// basic cube
+var geometry = new THREE.BoxGeometry( 1, 1, 1)
+var material = new THREE.MeshStandardMaterial( { color: "#82bfa1", flatShading: true, metalness: .5, roughness: 1 })
+var cube = new THREE.Mesh ( geometry, material )
+scene.add( cube )
 
-			const animate = function () {
-				requestAnimationFrame( animate );
+// wireframe cube
+var geometry = new THREE.BoxGeometry( 3, 3, 3)
+var material = new THREE.MeshBasicMaterial( {
+    color: "#008793", wireframe: true, transparent: true
+})
+var wireframeCube = new THREE.Mesh ( geometry, material )
+scene.add( wireframeCube )
 
-				cube.rotation.x += 0.08;
-				cube.rotation.y += 0.02;
+// ambient light
+var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.2)
+scene.add( ambientLight )
 
-				renderer.render( scene, camera );
-			};
+// point light
+var pointLight = new THREE.PointLight( 0xffffff, 1 );
+pointLight.position.set( 25, 50, 25 );
+scene.add( pointLight );
 
-			animate();
+
+function animate() {
+    requestAnimationFrame( animate )
+    cube.rotation.x += 0.04;
+    cube.rotation.y += 0.04;
+    wireframeCube.rotation.x -= 0.01;
+    wireframeCube.rotation.y -= 0.01;
+    renderer.render( scene, camera )
+}
+animate()
