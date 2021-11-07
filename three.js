f3c22f791f7110073b53e208f7303b30f245ac81
Jamon Dixon <jamonadixon@gmail.com>
2021-11-07 17:46:07 -0600

    remove sphere and poly

--- three.js
+++ three.js
@@ -27,25 +27,6 @@ var material = new THREE.MeshStandardMaterial( { color: "#82bfa1", flatShading: 
 var cube = new THREE.Mesh ( geometry, material )
 scene.add( cube )
 
-// polyhedron not working
-// const verticesOfCube = [
-//     -1,-1,-1,    1,-1,-1,    1, 1,-1,    -1, 1,-1,
-//     -1,-1, 1,    1,-1, 1,    1, 1, 1,    -1, 1, 1,
-// ];
-
-// const indicesOfFaces = [
-//     2,1,0,    0,3,2,
-//     0,4,7,    7,3,0,
-//     0,1,5,    5,4,0,
-//     1,2,6,    6,5,1,
-//     2,3,7,    7,6,2,
-//     4,5,6,    6,7,4
-// ];
-
-// var poly = new THREE.PolyhedronGeometry( verticesOfCube, indicesOfFaces, 6, 2 );
-// var materials = new THREE.MeshStandardMaterial( { color: "#82bfa1", flatShading: true, metalness: 0, roughness: 1 })
-// var hedron = new THREE.Mesh ( poly, materials )
-// scene.add( poly )
 
 // wireframe cube
 var geometry = new THREE.BoxGeometry( 3, 3, 3)
@@ -64,11 +45,7 @@ var pointLight = new THREE.PointLight( 0xffffff, 1 );
 pointLight.position.set( 25, 50, 25 );
 scene.add( pointLight );
 
-// sphere not working
-// const sphereGeometry = new THREE.SphereGeometry( 15, 32, 16 );
-// const materials = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-// const sphere = new THREE.Mesh( sphereGeometry, materials );
-// scene.add( sphere );
+
 
 
 function animate() {
